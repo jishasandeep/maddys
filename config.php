12b402af347e3d97b4e988e79938e92e56a199f2
jishasandeep <jishasandeep@gmail.com>
2017-06-18 19:22:19 +0530

    Adding to repo

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/image/');
+define('DIR_CACHE', 'C:/wamp/www/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/system/upload/');
+define('DIR_MODIFICATION', 'C:/wamp/www/system/modification/');
+define('DIR_LOGS', 'C:/wamp/www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dbadmin');
+define('DB_PASSWORD', 'dbadmin');
+define('DB_DATABASE', 'MCDB');
+define('DB_PREFIX', '');
